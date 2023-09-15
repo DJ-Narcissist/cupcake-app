852d546591b80e808656836898564c4b30b5b998
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-09-15 15:07:46 -0400

    app.py and model.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, request, url_for
+from models import Cupcake, db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] ='Cakes_for_less'
+
+connect_db(app)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    """Cupcake Model"""
+    __tablename__ = "cupcakes"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    flavor = db.Column(db.String(50), nullable = False)
+    size = db.Column(db.String, nullable = False)
+    rating  = db.Column(db.Float, nullable = False)
+    image = db.Column(db.String, nullable = False, default = "https://tinyurl.com/demo-cupcake")
+
+    
